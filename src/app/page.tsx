9e4c34499ffed7e63930e1ac7c201b89b5db5547
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2024-11-11 00:46:50 +0530

    2nd version of name display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [color, setColor] = useState('black')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setColor('#333')
+    }, 3000);
+
+    return () => clearTimeout(timer)
+
+  }, [])
+
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen w-screen bg-gray-100">
-        <h1 className="text-8xl font-bold">Sanjay Achar</h1>
-      </div>
+    <div
+      className="flex justify-center items-center h-screen w-screen bg-gray-100"
+    >
+      <h1
+        style={{ color, transition: "color is ease-in-out" }}
+        className="text-8xl md:text-9xl font-extrabold text-gray-900 drop-shadow-lg tracking-wider"
+      >
+        Sanjay Achar</h1>
     </div>
   );
 }
