d92fa6f8d98ccc93d886e4d2da6fa912200dbd71
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2024-11-12 08:55:16 +0530

    add svg icons for socials & github & mail & resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,53 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const [color, setColor] = useState('black')
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setColor('#333')
-    }, 3000);
-
-    return () => clearTimeout(timer)
-
-  }, [])
 
   return (
-    <div
-      className="flex justify-center items-center h-screen w-screen bg-gray-100"
-    >
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
       <h1
-        style={{ color, transition: "color is ease-in-out" }}
         className="text-8xl md:text-9xl font-extrabold text-gray-900 drop-shadow-lg tracking-wider"
       >
-        Sanjay Achar</h1>
+        Sanjay Achar
+      </h1>
+      <div className="flex flex-row gap-4 mt-8 pt-4">
+        <Image
+          priority
+          src='/social/twitter-x.svg'
+          height={50}
+          width={50}
+          alt="X.com"
+        />
+        <Image
+          priority
+          src='/social/discord-icon.svg'
+          height={50}
+          width={50}
+          alt="Discord Icon"
+        />
+        <Image
+          priority
+          src='/social/github.svg'
+          height={50}
+          width={50}
+          alt="Github Icon"
+        />
+        <Image
+          priority
+          src='/social/mail.svg'
+          height={50}
+          width={50}
+          alt="G-Mail Icon"
+        />
+        <Image
+          priority
+          src='/social/resume.svg'
+          height={50}
+          width={50}
+          alt="Resume"
+        />
+      </div>
     </div>
   );
 }
